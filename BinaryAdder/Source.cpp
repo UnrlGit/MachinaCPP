5554cee4e38b8192a2476d06af365fb01f3aa778
aronthompson <aronthompsonemail@gmail.com>
2019-03-30 03:28:52 +0100

    Shorter main

--- BinaryAdder/Source.cpp
+++ BinaryAdder/Source.cpp
@@ -4,11 +4,8 @@
 
 int main()
 {
-	int addition = BitwiseMath::Adder(22, 4);
-	int substraction = BitwiseMath::Substractor(200, 111);
-	
-	std::cout << "Addition of 22 and 4: " << addition << std::endl;
-	std::cout << "Substraction" << substraction << std::endl;
+	std::cout << "Addition of 22 and 4: " << BitwiseMath::Adder(22, 4) << std::endl;
+	std::cout << "Substraction" << BitwiseMath::Substractor(200, 111) << std::endl;
 
 	system("pause");
 }
